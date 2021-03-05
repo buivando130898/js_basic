a7dc203c6ab6f092e7077e632edfe7627e9afd98
BuiDo <buivando130898@gmail.com>
2021-03-05 09:50:29 +0700

    add word

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-console.log("hello github");
+
+for (i=0;i<=2;i++)
+    {
+        let hehe =3;
+        if (hehe == 3)
+            {
+                lol = "hello";
+            }
+        
+        console.log(lol);
+
+    }
+
+console.log(lol1);
+lol1 = "hello1111";
--- test2.js
+++ test2.js
@@ -1,1 +1,17 @@
-console.log("test barch");
+a=7;
+b=3;
+c= a++;
+
+console.log(typeof d);
+
+var test = "a" && "B" && "C";
+console.log( test);
+a="2";
+a=String(a);
+console.log(a.length);
+chuoi = "hehehehehehehehe"
+ar1 = ["test1", 2222, "hello"];
+ar2 = ["test2", 333, 444, "lol"];
+
+console.log(test.length);
+console.log(chuoi.indexOf("hehe",11))
